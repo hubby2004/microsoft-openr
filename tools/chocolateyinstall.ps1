f9e56b04b46eb43660a2e8d40bf30d1c88efe621
Ramdane <ramdane@ufl.edu>
2018-05-27 13:25:05 -0400

    Install script for Microsoft R Open

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'microsoft-openr'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = "https://mran.blob.core.windows.net/install/mro/3.4.4/microsoft-r-open-3.4.4.exe"
+$checkSum = "1E4C676D840EE847CB1A28709573D9A520EEF244746A3B0B97017CE9AACDB58F"
+
+$packageArgs = @{
+    packageName    = $packageName
+    fileType       = 'EXE'
+    checksum       = $checkSum
+    checksumType   = 'sha256'
+    url            = $url
+    silentArgs     = '/quiet'
+    validExitCodes = @(0)
+}
+
+
+Install-ChocolateyPackage @packageArgs
